f876e59804c840bdc3c6082df3e9f1c40e7a0ff9
valgarciav <vkgarcia@gmail.com>
2022-05-10 00:32:37 -0400

    Adding places controller

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,29 +1,8 @@
 // create and export express.Router()
-const router = require("express").Router();
+const router = require('express').Router()
 
-//make a first route in places.js
-router.get("/", (req, res) => {
-  res.send("GET /places");
-});
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
 
-// GET /places
-app.get("/", (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-  }
-  ];
-  res.render('places/index', { places })
-
-});
-
-module.exports = router;
+module.exports = router
