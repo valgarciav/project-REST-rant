6e3afdea0db85da09fe0e660918bc175026fabd3
valgarciav <vkgarcia@gmail.com>
2022-05-10 00:44:10 -0400

    Adding default view for layout

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,8 +1,8 @@
 // create and export express.Router()
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+router.get("/", (req, res) => {
+  res.send("GET /places");
+});
 
-module.exports = router
+module.exports = router;
--- views/default.jsx
+++ views/default.jsx
@@ -1,14 +1,14 @@
 //Import React
-const React = require('react')
+  const React = require('react')
 
-// sub function called Def that has one parameter called html
-function Def (html) {}
-
-//export the Def function i just created
-module.exports = Def
-
-//fill Def function with a return statement that includes the HTML skeleton of all the pages im going to include 
-function Def (html) {
+  // sub function called Def that has one parameter called html
+  function Def (html) {}
+  
+  //export the Def function i just created
+  module.exports = Def
+  
+  //fill Def function with a return statement that includes the HTML skeleton of all the pages im going to include 
+  function Def (html) {
     return (
         <html>
             <head>
