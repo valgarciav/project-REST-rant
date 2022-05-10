6b9a29b7c48a7d8716082f3f6fc383012d0c8c64
valgarciav <vkgarcia@gmail.com>
2022-05-10 00:25:39 -0400

    Adding 404 page route

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,14 +6,14 @@ function index () {}
 module.exports = index
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map(place) => {
       return (
         <div>
           <h2>{place.name}</h2>
           <img src={place.pic} alt={place.name}>
         </div>
-      )
-    })
+      
+    }
     return (
       <Def>
           <main>
