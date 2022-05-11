250b38af9c76059cf82bf7f36164573d3a96935d
valgarciav <vkgarcia@gmail.com>
2022-05-10 20:44:48 -0400

    New places can be added

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,9 +1,14 @@
 // create and export express.Router()
 const router = require("express").Router();
+const places = require("../models/places");
 
 //router
 router.get("/new", (req, res) => {
   res.render("places/new");
+});
+
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
 });
 
 // GET /places
@@ -25,6 +30,22 @@ router.get("/", (req, res) => {
     },
   ];
   res.render("places/index", { places });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 
+//when we send data with the POST verb, that data gets encrypted for its trip across the internet. Because it is protected this way while in transit, that makes it extra safe for usernames, passwords, and other sensitive data. However, it also means we will need an extra tool to decrypt that data for us.
+
+
+app.use(express.urlencoded({ extended: true }))
+
+
 // Controllers & Routes
 app.use("/places", require("./controllers/places"));
 
