cbe02dc0afd08b7956987ad372f46a47ab9f80fa
valgarciav <vkgarcia@gmail.com>
2022-06-05 19:42:28 -0400

    update show.jsx
    editing comments code in show page when adding previously made comments.

--- views/Places/show.jsx
+++ views/Places/show.jsx
@@ -53,7 +53,6 @@ function show(data) {
             <h1>{data.place.name}</h1>
             <h2>Rating</h2>
             {rating}
-            <br />
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
@@ -117,40 +116,6 @@ function show(data) {
     </Def>
   );
 }
-
-/**
- * 
- * 
-const React = require('react')
-const Def = require('../default')
-
-function show(data) {
-    return (
-        <Def>
-          <main>
-            <h1>{ data.place.name }</h1>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-  Edit
-</a>     
-<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-  <button type="submit" className="btn btn-danger">
-    Delete
-  </button>
-</form> 
-     
-
-
-          </main>
-        </Def>
-    )
-}
-
-
-
- * 
- * 
- * 
- */
 
 module.exports = show;
 
